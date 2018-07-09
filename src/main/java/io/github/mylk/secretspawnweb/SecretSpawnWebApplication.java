daa963c3c9a671ae1d54b0b787d3605db81e7b47
Kostas Milonas <milonas.ko@gmail.com>
2018-07-10 01:31:00 +0300

    initial commit

---
+++ src/main/java/io/github/mylk/secretspawnweb/SecretSpawnWebApplication.java
@@ -0,0 +1,12 @@
+package io.github.mylk.secretspawnweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretSpawnWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretSpawnWebApplication.class, args);
+	}
+}
