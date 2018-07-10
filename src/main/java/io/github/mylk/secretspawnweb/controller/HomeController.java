f99de3b6a6133eb63c6ec6779516291632b53014
Kostas Milonas <milonas.ko@gmail.com>
2018-07-11 00:20:33 +0300

    integrates with secret-spawn application

--- src/main/java/io/github/mylk/secretspawnweb/controller/HomeController.java
+++ src/main/java/io/github/mylk/secretspawnweb/controller/HomeController.java
@@ -1,14 +1,22 @@
 package io.github.mylk.secretspawnweb.controller;
 
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import io.github.mylk.secretspawn.SecretSpawnApplication;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = "/", method = { RequestMethod.GET})
-    public String index() {
+    @RequestMapping(value = "/", method = { RequestMethod.GET, RequestMethod.POST })
+    public String index(Model model, HttpServletRequest request) {
+        if (request.getMethod().equals("POST")) {
+            String[] args = new String[]{"-format", "hackish"};
+            SecretSpawnApplication.main(args);
+        }
         return "index";
     }
 }
