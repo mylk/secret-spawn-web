a74e811aa07bc8fbca9da70bda72ce65841b95c5
Kostas Milonas <milonas.ko@gmail.com>
2018-07-12 02:23:42 +0300

    displays results in web interface instead of application stdout

--- src/main/java/io/github/mylk/secretspawnweb/controller/HomeController.java
+++ src/main/java/io/github/mylk/secretspawnweb/controller/HomeController.java
@@ -1,22 +1,53 @@
 package io.github.mylk.secretspawnweb.controller;
 
-import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import io.github.mylk.secretspawn.SecretSpawnApplication;
+import io.github.mylk.secretspawn.manager.SettingsManager;
+import io.github.mylk.secretspawn.manager.SecretManager;
+import io.github.mylk.secretspawn.model.Settings;
+import io.github.mylk.secretspawn.model.Secret;
+
+import io.github.mylk.secretspawnweb.model.SecretPreferences;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = "/", method = { RequestMethod.GET, RequestMethod.POST })
-    public String index(Model model, HttpServletRequest request) {
-        if (request.getMethod().equals("POST")) {
-            String[] args = new String[]{"-format", "hackish"};
-            SecretSpawnApplication.main(args);
-        }
+    @GetMapping("/")
+    public String index() {
         return "index";
+    }
+
+    @PostMapping("/")
+    public String generate(@ModelAttribute SecretPreferences secretPreferences, Model model) {
+        String[] args = new String[]{
+            "-length", secretPreferences.getLength().toString(),
+            "-source", secretPreferences.getSource(),
+            "-format", secretPreferences.getFormat(),
+        };
+        SettingsManager settingsManager = new SettingsManager();
+        settingsManager.defineOptions();
+
+        Settings settings = new Settings();
+        try {
+            settings = settingsManager.parse(args);
+
+            SecretManager secretManager = new SecretManager();
+            secretManager.setSettings(settings);
+            Secret secret = new Secret();
+            secret = secretManager.spawn();
+
+            model.addAttribute("secret", secret.getContentTransformed());
+            model.addAttribute("title", secret.getTitle());
+            model.addAttribute("url", secret.getUrl());
+            model.addAttribute("content", secret.getContentPlain());
+        } catch (Exception ex) {
+            model.addAttribute("error", ex.getMessage());
+        }
+
+        return "result";
     }
 }
---
+++ src/main/java/io/github/mylk/secretspawnweb/model/SecretPreferences.java
@@ -0,0 +1,32 @@
+package io.github.mylk.secretspawnweb.model;
+
+public class SecretPreferences {
+
+    private Integer length;
+    private String source;
+    private String format;
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public void setLength(Integer length) {
+        this.length = length;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+}
